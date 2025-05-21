1d1e4178380e1cae41563fcd5208a6e0ab9b6dde
kunledarams2 <daramolaadekunle15@gmail.com>
2025-05-21 17:28:16 +0100

    add privacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // base: '/ThriftPay_Web/',
+
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  // base: '/ThriftPay_Web/',
+  build: {
+    outDir: 'dist',
+  },
+ 
+});
 
-})
