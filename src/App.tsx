059d39ff31c5aea48b781a141e6d934ec71e16bb
kunledarams2 <daramolaadekunle15@gmail.com>
2025-05-21 18:04:56 +0100

    add privacy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import TopNav from "./components/TopNav";
 import Footer from "./components/Footer";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const redirectedRef = useRef(false); // guard
+
+  useEffect(() => {
+    if (redirectedRef.current) return;
+
+    if (
+      location.search.includes("~and~") ||
+      location.search.includes("&/&") ||
+      location.search.length > 20
+    ) {
+      redirectedRef.current = true;
+      navigate(location.pathname, { replace: true });
+    }
+  }, [location, navigate]);
+
   return (
     <>
       <TopNav />
@@ -14,3 +32,31 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     // Clean up garbage query strings globally
+//     if (
+//       location.search.includes("~and~") ||
+//       location.search.includes("&/&") ||
+//       location.search.length > 20
+//     ) {
+//       // Rebuild the clean path (strip query and hash)
+//       const cleanPath = location.pathname;
+//       navigate(cleanPath, { replace: true });
+//     }
+//   }, [location, navigate]);
+
+//   return (
+//     <>
+//       <TopNav />
+//       <Outlet />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,7 @@ const router = createBrowserRouter([
     // path: "/",
     // element: <ThriftPayLandingPage />,
     // // errorElement: <ErrorPage />,
+
     children: [
       {
         path: "/",
